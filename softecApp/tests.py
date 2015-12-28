a683df2d23f5522e11adf8e80c998b4c75dc85ae
Luis Naranjo <luisnaranjo733@gmail.com>
2015-12-27 18:01:55 -0800

    added skel for tests

--- softecApp/tests.py
+++ softecApp/tests.py
@@ -1,3 +1,39 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import *
+
+class StaffTests(TestCase):
+    pass
+
+class RestaurantTests(TestCase):
+    pass
+
+class CustomerTests(TestCase):
+    pass
+
+class Customer_RestaurantTests(TestCase):
+    pass
+
+class SalesOrderTests(TestCase):
+    pass
+
+class ProductTests(TestCase):
+    pass
+
+class ProductInventoryTests(TestCase):
+    pass
+
+class SalesOrder_ProductTests(TestCase):
+    pass
+
+class VendorTests(TestCase):
+    pass
+
+class Vendor_ProductTests(TestCase):
+    pass
+
+class PurchaseOrderTests(TestCase):
+    pass
+
+class PurchaseOrder_ProductTests(TestCase):
+    pass
